6dafe1b744651df1153222bd3d9d9aa2dd0d22ec
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-07-27 23:16:53 +0300

    Connect DB

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 const express = require('express');
+const dbConfig = require('./config/mongoose.config');
 const config = require('./config/config')[process.env.NODE_ENV];
+
 const app = express();
 
 // Setup express server
-require('./config/express-config')(app);
+require('./config/express.config')(app);
 
 app.get('/', (req, res) => {
-    console.log(req.query);
-    res.json({ message: 'Some message' })
+    res.send('<h1>REST Server Boilerplate</h1>');
 });
 
-app.listen(
-    config.PORT,
-    () => console.log('Server is listening on port: ' + config.PORT)
-);
+dbConfig(config.MONGODB_URL)
+    .then(() => {
+        console.log('DB connected!');
+        app.listen(config.PORT, () => console.log('Server is listening on port: ' + config.PORT))
+    }).catch(err => {
+        console.log('DB connection error: ', err);
+    });
