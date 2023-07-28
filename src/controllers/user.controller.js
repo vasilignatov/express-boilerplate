ab195e92e916a6397c67e8609d69f9c833d2dac2
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-07-28 17:33:33 +0300

    Added user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,41 @@
+const httpStatus = require('http-status');
+const userService = require('../services/user.service');
+
+const createUser = async (req, res) => {
+    const user = await userService.createUser(req.body);
+    console.log("USER: ", user);
+    res.status(httpStatus.CREATED).send(user);
+}
+
+const getUsers = (req, res) => {
+    // TODO: query string filtering
+}
+
+const getUser = async (req, res) => {
+    const user = await userService.getUserById(req.params.userId);
+    if (!user) {
+        throw new Error('User not found');
+    }
+    res.send(user);
+}
+
+
+const deleteUser = async (req, res) => {
+    const user = await userService.deleteUserById(req.params.userId);
+    console.log(user);
+    res.send(user);
+}
+
+const updateUser = async (req, res) => {
+    const user = await userService
+        .updateUserById(req.params.userId, req.body);
+
+    res.send(user);
+}
+
+module.exports = {
+    createUser,
+    getUser,
+    deleteUser,
+    updateUser
+}
