259c8099f87c40c9967cf8ab30419ec3417aa439
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-03 23:55:48 +0300

    Edited user service

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,21 +2,25 @@ const User = require('../models/User');
 const AppError = require('../utils/AppError');
 const httpStatus = require('http-status');
 
-const createUser = async (userData) => {
-    if (await User.isUsernameTaken(userData.username)) {
-        throw new AppError('Username already taken!', httpStatus.BAD_REQUEST);
-    }
-    return User.create(userData);
-}
-
 const getUserById = async (id) => {
     return User.findById(id);
+}
+
+const getUserByEmail = async (email) => {
+    return User.findOne({ email });
+}
+
+const createUser = async (userData) => {
+    if (await User.isEmailTaken(userData.email)) {
+        throw new AppError('Email already taken!', httpStatus.BAD_REQUEST);
+    }
+    return User.create(userData);
 }
 
 const updateUserById = async (id, userData) => {
     const user = await getUserById(id);
 
-    if(!user) throw new AppError('User not fount', httpStatus.NOT_FOUND);
+    if (!user) throw new AppError('User not fount', httpStatus.NOT_FOUND);
 
     Object.assign(user, userData);
     await user.save();
@@ -28,8 +32,9 @@ const deleteUserById = async (id) => {
 }
 
 module.exports = {
-    createUser,
     getUserById,
+    getUserByEmail,
+    createUser,
     updateUserById,
     deleteUserById
 }
