ac5ebb776abf9c520d5c28b3329e6bed4f6f5df4
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-03 17:13:58 +0300

    Update error.js to handle mongoose errors

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,9 +1,13 @@
 const httpStatus = require('http-status');
 const config = require('../config/config');
+const { MongooseError } = require('mongoose');
 
 const errorHandler = (err, req, res, next) => {
-    
     let { message, statusCode } = err;
+
+    if (err instanceof MongooseError) {
+        statusCode = httpStatus.BAD_REQUEST;
+    }
 
     if (config.env == 'production' && err.isOperational == false) {
         statusCode = httpStatus.INTERNAL_SERVER_ERROR;
@@ -12,7 +16,7 @@ const errorHandler = (err, req, res, next) => {
 
     res
         .status(statusCode)
-        .send({
+        .json({
             statusCode,
             message
         });
