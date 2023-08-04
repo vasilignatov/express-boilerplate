96140666412cca973204d04a2bcdf5ef908bf568
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-04 17:23:42 +0300

    Add logout and refreshTokens functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,39 +2,44 @@ const httpStatus = require('http-status');
 const catchAsync = require('../utils/catchAsync');
 const userService = require('../services/user.service');
 const authService = require('../services/auth.service');
+const tokenService = require('../services/token.service');
 const AppError = require('../utils/AppError');
 
 const register = catchAsync(async (req, res) => {
     const { email, password } = req.body;
-    const user = await userService.createUser({email, password});
-    
-    //TODO: GENERATE TOKENS;
-    let token;
-   
+    const user = await userService.createUser({ email, password });
+    const token = await tokenService.generateAuthTokens(user);
+
     res
         .status(httpStatus.CREATED)
-        .json({user, token});
-        // send token and user;
+        .json({ user, token });
 });
 
 const login = catchAsync(async (req, res) => {
     const { email, password } = req.body;
 
     const user = await authService.loginLocal(email, password);
-    
-    // TODO: TOKEN
-    let token;
+    const token = await tokenService.generateAuthTokens(user);
 
-    res.json({user, token});
+    res.json({ user, token });
 });
 
 const logout = catchAsync(async (req, res) => {
+    await authService.logout(req.body.refreshToken);
+    res
+        .status(httpStatus.NO_CONTENT)
+        .json({ ok: true });
+});
 
+const refreshTokens = catchAsync( async (req, res) => {
+    const tokens = await authService.refreshAuth(req.body.refreshToken);
+    res.json({tokens});
 });
 
 
 module.exports = {
     register,
     login,
-    logout
+    logout,
+    refreshTokens
 }
