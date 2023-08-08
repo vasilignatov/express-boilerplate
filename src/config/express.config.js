337b648886de2c85fbe24d465ae0e2b3973f93e6
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-08 22:35:48 +0300

    Added compression and coments in express config

--- src/config/express.config.js
+++ src/config/express.config.js
@@ -3,15 +3,37 @@ const helmet = require('helmet');
 const cors = require('cors');
 const routes = require('../routes/routes');
 const errorHandler = require('../middlewares/error');
+const passport = require('passport');
+const jwtStrategy = require('../config/passport.config');
+const compression = require('compression');
 
 function expressConfig(app) {
+
+    // set HTTP headers properly
+    app.use(helmet());
+
+    // Middleware to enable CORS
     app.use(cors());
+
+    // Middleware to parse requests with JSON payloads
     app.use(express.json());
+
+    // Middleware to parse URL-encoded da
     app.use(express.urlencoded({
         extended: true
     }));
-    app.use(helmet());
+
+    // Middleware to compress response 
+    app.use(compression());
+
+    // Setup passport (use JWT)
+    app.use(passport.initialize());
+    passport.use('jwt', jwtStrategy);
+
+    // Add modular router
     app.use(routes);
+    
+    // Global Error Handler
     app.use(errorHandler);
 }
 
