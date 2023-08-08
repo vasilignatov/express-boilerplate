d83c8faf1b6c18a5e1786daab78fbc96acd47238
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-08 22:36:34 +0300

    Edited passport configuration

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,23 +1,34 @@
 const passport = require('passport');
 const { ExtractJwt, Strategy: JwtStrategy } = require('passport-jwt');
-const { JWT_SECRET } = require('./config');
+const config = require('./config')[process.env.NODE_ENV];
 const User = require('../models/User');
+const AppError = require('../utils/AppError');
+const httpStatus = require('http-status');
 
-const options = {
+const opt = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: JWT_SECRET
+    secretOrKey: config.JWT_SECRET
 }
 
-
-passport.use(options, (payload, done) => {
+const verify = async (payload, done) => {
     try {
+        if (payload.type !== 'access') {
+            throw new AppError('Invalid token type', httpStatus.UNAUTHORIZED);
+        }
+
         const user = await User.findById(payload.sub);
-        if(!user) {
+
+        if (!user) {
             return done(null, false);
         }
+
         done(null, user);
-    } catch(err) {
+
+    } catch (err) {
         done(err, false);
     }
-});
+};
 
+const jwtStrategy = new JwtStrategy(opt, verify);
+
+module.exports = jwtStrategy;
