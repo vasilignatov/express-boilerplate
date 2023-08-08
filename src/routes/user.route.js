b713c64016cb433799e9dac95d1ddd29643454dc
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-08-08 22:39:31 +0300

    Added auth rmiddleware to user routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const userController = require('../controllers/user.controller');
+const { auth, isAdmin } = require('../middlewares/auth');
 
+router.post('/',auth, userController.createUser);
 
-router.post('/', userController.createUser);
-
-router.get('/:userId', userController.getUser);
-router.put('/:userId',userController.updateUser);
-router.delete('/:userId',userController.deleteUser);
-
+router.get('/:userId', auth, userController.getUser);
+router.put('/:userId', auth, userController.updateUser);
+router.delete('/:userId', auth, userController.deleteUser);
 
 
 module.exports = router;
